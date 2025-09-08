bb36a7b7490f63fcd1d36fa93b9eadf54191afb7
sasha_ <oopavlyk@gmail.com>
2025-09-08 23:36:17 +0200

    init

---
+++ note-app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5006)
